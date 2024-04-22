560334cf4f2b873acfa95f0d3489452fcbd21840
Paola Astudillo <alu0101337418@ull.edu.es>
2024-04-22 10:27:42 +0000

    readme

--- src/practica9/app.ts
+++ src/practica9/app.ts
@@ -4,12 +4,19 @@ import cardRoutes from '../practica9/CardRoute.js';
 /**
  * Aplicación Express para gestionar las solicitudes relacionadas con las cartas Magic.
  */
+
 const app = express();
 
 // Middleware para analizar el cuerpo de las solicitudes en formato JSON
+//se utiliza para analizar el cuerpo de las solicitudes entrantes con formato JSON.
+//le dices a express que analice el cuerpo de las solicitudes de formato json y 
+//lo hace accesible en el req.body, para que trabaje con esos datos en sus manejadores de rutas
 app.use(express.json());
 
 // Rutas de la API para las operaciones sobre las cartas
+//aqui el enrutador se monta en la aplicacion EXpress usando api/ como prefijio de laa ruta
+///seria todo api/cards y cardroutes es el enrutador que quiero montar
+//resumen todas la solicitudes que empiecen por api seran manejadas por cardroutes
 app.use('/api', cardRoutes);
 
 // Puerto en el que el servidor Express escucha las solicitudes entrantes
